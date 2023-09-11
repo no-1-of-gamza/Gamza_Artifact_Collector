85c601a72dcf8fce8a845b2efa7efaa4ee97463d
( DO DREAM DEVELOPMENT) DEVELOPER D 실행하고 꿈꾸고 발전하는개발자.. 가 되고싶습니다 <68416184+silver-liq9118@users.noreply.github.com>
2023-09-11 15:50:40 +0900

    Window 7 exe language update
    Kor -> Eng

--- Window_7/Gamza_Artifact_Collector_py_Win7/Gamza_Artifact_Collector.py
+++ Window_7/Gamza_Artifact_Collector_py_Win7/Gamza_Artifact_Collector.py
@@ -1,140 +1,205 @@
 # -*- coding: utf-8 -*-
 import print_message
 from option import option_set
-from a_trash_bin_data import trashbin_data
 
+from a_recyclebin_data import RecycleBin
 from a_system_information import Systeminfo_Collector
-from a_resistry_data import Registry_config, Registry_Collector
-from a_browser_history import Browser_Config, Browser_Collector
+from a_registry_data import Registry_config, Registry_Collector
 from a_event_log import EventLog_Config, EventLog_Collector
+from a_browser_history import Browser_Config, Browser_Collector
+from a_extension import Extension
 
 from datetime import datetime
 import os
-import optparse
+import argparse
 import multiprocessing
 import traceback
 
 
+def Browser_History(inspect_path, Window_version, InstallPath_system_root, profile_list, UTC):
+    try:
+        os.mkdir("%s//Browser_History" % inspect_path)
+        Browser_History_result_path = str(inspect_path) + "\\Browser_History"
+
+        Browser_config = Browser_Config(Window_version, InstallPath_system_root, profile_list)
+        Browser_artifact_path = Browser_config.run()
+
+        Browser_collector = Browser_Collector(Browser_History_result_path, UTC)
+        Browser_collector.collect(Browser_artifact_path)
+        print "Browser history...complete"
+    except Exception as e:
+        print e
+        pass
+
+
+def Event_log(inspect_path, Window_version, InstallPath_system_root, UTC):
+    try:
+        os.mkdir("%s//Event" % inspect_path)
+
+        Event_log_result_path = str(inspect_path) + "\\Event"
+
+        EventLog_config = EventLog_Config(Window_version, InstallPath_system_root)
+        EventLog_artifact_path = EventLog_config.run()
+
+        EventLog_collector = EventLog_Collector(Event_log_result_path, UTC)
+        EventLog_collector.collect(EventLog_artifact_path)
+        print "Event log...complete"
+    except Exception as e:
+        print e
+        pass
+
+
+def Registry_Data(inspect_path, Window_version, profile_list, UTC):
+    try:
+        os.mkdir("%s//Registry" % inspect_path)
+
+        Register_result_path = str(inspect_path) + "\\Registry"
+
+        Register_config = Registry_config(Window_version, profile_list)
+        Resgister_artifact_path = Register_config.run()
+
+        Register_collector = Registry_Collector(Register_result_path, UTC)
+        Register_collector.collect(Resgister_artifact_path)
+        print "Registry data...complete"
+    except Exception as e:
+        print e
+        pass
+
+
+def Recycle_bin(inspect_path, UTC):
+    try:
+        os.mkdir("%s//Trash_bin" % inspect_path)
+        Trashbin_result_path = str(inspect_path) + "\\Trash_bin"
+
+        artifact = RecycleBin(Trashbin_result_path, UTC)
+        artifact.check_drive()
+
+        artifact.collect()
+
+        pool = multiprocessing.Pool(processes=4)
+        pool.apply_async(artifact.dump)
+        print "Recycle bin data...complete"
+        pool.close()
+        pool.join()
+    except Exception as e:
+        print e
+        pass
+
+
+def Extension_files(inspect_path, UTC, target_extensions):
+    os.mkdir("%s//Extension_file" % inspect_path)
+    Trashbin_result_path = str(inspect_path) + "\\Extension_file"
+
+    Extension_files_artifact = Extension(Trashbin_result_path, UTC, target_extensions)
+    Extension_files_artifact.create_dir(Trashbin_result_path, Extension_files_artifact.drive_list)
+    Extension_files_artifact.collect()
+    with multiprocessing.Pool(processes=4) as pool:
+        pool.map(Extension_files_artifact.dump, Extension_files_artifact.src_dst)
+
+    print "Extension files...complete"
+
+
 def main():
     try:
-        # 웰컴메세지
+        # Welcome message
         print_message.print_welcome_message()
 
-        # 옵션 불러오기
-        parser = optparse.OptionParser(description="Gamza Scanner Artifact Collector")
-        parser.add_option("-a", "--artifact", action="store_true", help="Use this option to collect specific artifacts.")
-        parser.add_option("-s", "--search", action="store_true", help="Use this option to search for specific file extensions.")
+        # Load options
+        args = option_set()
 
-        (options, args) = parser.parse_args()
-
-        # 수사자, 사건 케이스 이름 입력
-        print("\nPlease Insert Inspector name")
-        Inspector_name = raw_input("Inspector_name: ")
-        Victim_name = raw_input("Victim_name: ")
+        # Enter Inspector and Victim names
+        print "\nPlease Insert Inspector name"
+        Inspector_name = raw_input("Inspector name: ")
+        Victim_name = raw_input("Victim name: ")
 
         if Inspector_name == "":
-            print("Inspector name is none")
+            print "Inspector name is none"
             Inspector_name = "unknown"
         if Victim_name == "":
-            print("Victim name is none")
+            print "Victim name is none"
             Victim_name = "unknown"
 
-        # 현재시간
+        # Current time
         date_time = datetime.today().strftime("%Y/%m/%d %H:%M:%S")
-        # 폴더 생성시간
+        # Folder creation time
         date_time_save_folder = datetime.today().strftime("%Y_%m_%d_%H_%M_%S")
 
-        print("Current Time: {}".format(date_time))
+        print "Current Time: %s" % date_time
 
-        # 현재 작업 디렉토리 경로에 폴더 생성
+        # Folder creation
         current_dir = os.getcwd()
-        print("Maked folder {}".format(current_dir))
-        print("Folder name: {}".format(date_time_save_folder + "_" + Inspector_name + "_" + Victim_name))
-        inspect_path = "{}".format(date_time_save_folder + "_" + Inspector_name + "_" + Victim_name)
+        print "Made folder %s" % current_dir
+        print "Folder name: %s_%s_%s" % (date_time_save_folder, Inspector_name, Victim_name)
+        inspect_path = "%s_%s_%s" % (date_time_save_folder, Inspector_name, Victim_name)
         os.mkdir(inspect_path)
 
-        artifact = options.artifact
-        search_file = options.search
-
-        # system information 아티팩트 불러오기 (기본)
+        artifact = args.artifact
+        target_extensions = args.file
+        # Collect system information artifact (default)
         system_information_collector = Systeminfo_Collector(inspect_path)
         system_information = system_information_collector.collect()
 
-        user_name=[system_information['ComputerName']]
-        UTC=system_information['Timezone']
+        UTC = system_information['Timezone']
         Window_version = system_information["ProductName"]
         InstallPath_system_root = system_information["InstallPath"]
-        profile_list= system_information["UserProfile"]
+        profile_list = system_information["UserProfile"]
 
+        print "\nPlease Choose and Insert Option Collecting Artifact [1] or Collecting Specific Extension File [2]"
+        function_choice = raw_input("Function:")
 
-        if artifact is None:
-            #Browser_History 
-            os.mkdir("{0}//Browser_History".format(inspect_path))
-            Browser_History_result_path = "{0}\\Browser_History".format(inspect_path)
+        if function_choice == "":
+            print "All of Artifact Collecting..."
+            artifact = None
 
-            Browser_config = Browser_Config(Window_version, InstallPath_system_root, profile_list)
-            Browser_artifact_path = Browser_config.run()
+        elif function_choice == "1":
+            artifact = []
+            print "Insert Collecting Artifact you want"
+            input_artifact = raw_input("Artifact: ")
+            artifact = input_artifact.split()
+            print artifact
 
-            Browser_collector = Browser_Collector(Browser_History_result_path, UTC)
-            Browser_collector.collect(Browser_artifact_path)
+        elif function_choice == "2":
+            target_extensions = []
+            print "Insert Collecting File you want"
+            target_extensions_input = raw_input("File extension: ")
+            target_extensions = target_extensions_input.split()
+            print target_extensions
+            if not target_extensions:
+                print "No file extension entered"
 
-            # Event_Log
-            os.mkdir("{}/Event".format(inspect_path))
+        # Collect all default artifacts if no specific options are provided
+        if (artifact is None or not artifact) and target_extensions is None:
+            print "All of Artifact Collecting..."
+            Browser_History(inspect_path, Window_version, InstallPath_system_root, profile_list, UTC)
+            Event_log(inspect_path, Window_version, InstallPath_system_root, UTC)
+            Registry_Data(inspect_path, Window_version, profile_list, UTC)
+            Recycle_bin(inspect_path, UTC)
 
-            Event_log_result_path = "{}/Event".format(inspect_path)
+        elif artifact:
 
-            EventLog_config = EventLog_Config(Window_version, InstallPath_system_root)
-            EventLog_artifact_path = EventLog_config.run()
+            try:
+                if 'b' in artifact:
+                    Browser_History(inspect_path, Window_version, InstallPath_system_root, profile_list, UTC)
+                if 'e' in artifact:
+                    Event_log(inspect_path, Window_version, InstallPath_system_root, UTC)
+                if 'r' in artifact:
+                    Registry_Data(inspect_path, Window_version, profile_list, UTC)
+                if 't' in artifact:
+                    Recycle_bin(inspect_path, UTC)
 
-            EventLog_collector = EventLog_Collector(Event_log_result_path, UTC)
-            EventLog_collector.collect(EventLog_artifact_path)
+            except Exception as e:
+                pass
 
-            # Register_Data
-            os.mkdir("{}/Registry".format(inspect_path))
+        if target_extensions:
+            Extension_files(inspect_path, UTC, target_extensions)
 
-            Register_result_path = "{0}\\Registry".format(inspect_path)
+        print "Success All Task"
+        raw_input("Press Enter to exit the program.")
 
-            Register_config = Registry_config(Window_version, user_name)
-            Resgister_artifact_path = Register_config.run()
-
-            Register_collector = Registry_Collector(Register_result_path, UTC)
-            Register_collector.collect(Resgister_artifact_path)
-
-            # Trashbin_Data
-            trashbin_data()
-        elif artifact is True:
-            if "b" in artifact:
-                browser_history()
-
-            if "e" in artifact:
-                event_log()
-
-            if "r" in artifact:
-                print("Register")
-            if "t" in artifact:
-                trashbin_data()
-
-        # Extension
-        if search_file is True:
-            if "doc" in search_file:
-                print("doc")
-            if "xls" in search_file:
-                print("xls")
-            if "txt" in search_file:
-                print("txt")
-            if "pdf" in search_file:
-                print("pdf")
-            if "zip" in search_file:
-                print("zip")
-            if "exe" in search_file:
-                print("exe")
-            if "lnk" in search_file:
-                print("lnk")
     except Exception as e:
-        print("오류가 발생했습니다:")
-        print(traceback.format_exc())
-        os.system('pause')
-        raw_input("엔터 키를 누르면 프로그램을 종료합니다.")
+        print "An error occurred:"
+        print traceback.format_exc()
+        raw_input("Press Enter to exit the program.")
 
 
 if __name__ == "__main__":
